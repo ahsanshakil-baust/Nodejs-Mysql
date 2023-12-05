fe744adbc3aaaf494695e747962699c258c433b4
Ahsan Shakil <ahsanshakil.baust@gmail.com>
2023-12-05 22:36:18 +0600

    function added

--- postfrontend/app/pages/home/HomePage.tsx
+++ postfrontend/app/pages/home/HomePage.tsx
@@ -2,10 +2,22 @@
 
 import { TextField, TextareaAutosize, Button } from "@mui/material";
 import { HomeWrapper } from "./style";
-
-const handleSubmit = (e: any) => {};
+import { useState } from "react";
 
 const HomePage = () => {
+    const [postDetails, setPostDetails] = useState({
+        title: "",
+        post: "",
+    });
+
+    const handleSubmit = (e: any) => {};
+
+    const changeHandler = (e: any) => {
+        const name = e.target.name;
+        const value = e.target.value;
+        setPostDetails((prevState) => ({ ...prevState, [name]: value }));
+    };
+
     return (
         <HomeWrapper>
             <TextField
@@ -13,12 +25,18 @@ const HomePage = () => {
                 id="outlined-basic"
                 label="Title"
                 variant="outlined"
+                onChange={changeHandler}
+                value={postDetails.title}
+                name="title"
             />
             <TextareaAutosize
                 style={{ width: "100%", marginTop: "10px", outline: "none" }}
                 maxRows={20}
                 minRows={5}
                 placeholder="Your Post..."
+                onChange={changeHandler}
+                value={postDetails.post}
+                name="post"
             />
 
             <Button
