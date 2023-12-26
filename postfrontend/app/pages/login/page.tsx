221b99993ace215571785cf3d9462714ac4351c6
ahsanshakil-baust <ahsanshakil.baust@gmail.com>
2023-12-26 16:25:36 +0600

    login working

--- postfrontend/app/pages/login/page.tsx
+++ postfrontend/app/pages/login/page.tsx
@@ -1,5 +1,9 @@
 const page = () => {
-  return <div>LogIn</div>;
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <input type="text" name="email" placeholder="Enter your Email:" />
+    </div>
+  );
 };
 
 export default page;
